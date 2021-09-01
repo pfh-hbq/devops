b1bd0d3137fd17e4df289ea84c3213c4b148048f
pfh-hbq <pfh.hbq@gmail.com>
2021-09-01 21:31:05 +0300

    DevOps lab 04

--- terraform/vb/vb.tf
+++ terraform/vb/vb.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     virtualbox = {
-      source  = "terra-farm/virtualbox"
+      source = "terra-farm/virtualbox"
       version = "0.2.1"
     }
   }
 }
 
 resource "virtualbox_vm" "node" {
-  count  = 2
-  name   = format("node-%02d", count.index + 1)
-  image  = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
-  cpus   = 1
+  count = 2
+  name = format("node-%02d", count.index + 1)
+  image = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus = 1
   memory = "1024 mib"
 
   network_adapter {
-    type           = "hostonly"
+    type = "hostonly"
     host_interface = "vboxnet1"
   }
 }
